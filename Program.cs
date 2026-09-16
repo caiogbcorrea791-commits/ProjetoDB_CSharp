568dcdeca78fcc613411abe2368d5e372eacfbfa
★Caio Gomes★ <caiogbcorrea791@gmail.com>
2026-09-16 13:51:16 -0300

    Implementação do menu

--- Program.cs
+++ Program.cs
@@ -8,3 +8,58 @@ using (MySqlConnection conexao = new MySqlConnection(connectionString))
     conexao.Open();
     Console.WriteLine("Conectado!");
 }
+
+
+int opcao = 0;
+
+while (opcao != 6)
+{   
+    Console.Clear();
+    
+    Console.WriteLine("|=================================================");
+    Console.WriteLine("|====================  MENU  =====================");
+    Console.WriteLine("|=================================================");
+    Console.WriteLine("| 1 - Cadastrar Jogo");
+    Console.WriteLine("| 2 - Listar Jogos");
+    Console.WriteLine("| 3 - Buscar Jogos");
+    Console.WriteLine("| 4 - Atualizar Jogo");
+    Console.WriteLine("| 5 - Deletar Jogo");
+    Console.WriteLine("| 6 - Sair");
+    Console.WriteLine("|=================================================");
+
+    Console.Write("| Digite sua escolha: ");
+    int.TryParse(Console.ReadLine(), out opcao);
+
+    while (opcao < 1 || opcao > 6)
+    {
+        Console.WriteLine("| Opção inválida!");
+        Console.Write("| Digite novamente: ");
+        int.TryParse(Console.ReadLine(), out opcao);
+    }
+
+    if (opcao == 1)
+    {
+        Console.WriteLine();
+    }
+    else if (opcao == 2)
+    {
+        Console.WriteLine();
+    }
+    else if (opcao == 3)
+    {
+        Console.WriteLine();
+    }
+    else if (opcao == 4)
+    {
+        Console.WriteLine();
+    }
+    else if (opcao == 5)
+    {
+        Console.WriteLine();
+    }
+    else if (opcao == 6)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Saindo...");
+    }
+}
--- obj/Debug/net10.0/ProjetoDB_CSharp.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProjetoDB_CSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoDB_CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9f9161363ec68a62c51bf3ad0cb5ff9488ed442")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee44dc1e79f485f24736edfcd31369a9582f9044")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoDB_CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoDB_CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
