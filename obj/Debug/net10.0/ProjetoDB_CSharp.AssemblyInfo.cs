68960667d6754c0d76cb0d2e1bb2af7132280b34
Vinicios Alves <viniciosconte045@gmail.com>
2026-09-17 14:38:04 -0300

    Perguntas

--- obj/Debug/net10.0/ProjetoDB_CSharp.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProjetoDB_CSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoDB_CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf7decd830032d52f4f17c82f170b14d397c4543")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d21c0b010683c29a18ca9d66219ab299914b716d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoDB_CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoDB_CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
